053d26464899e06545c36d587a5e5ee45c96f0da
fyrepaul <paul@fyresite.com>
2018-04-03 19:38:25 -0700

    updates

--- javascript/editor.js
+++ javascript/editor.js
@@ -2,6 +2,13 @@
   class Editor extends root.Context {
     constructor() {
       super();
+      // Block index bits
+      // First bit = upper right corner set
+      // Second bit = lower right corner set
+      // Third bit = lower left corner set
+      // Forth bit = upper left corner set
+
+
     }
   }
 
