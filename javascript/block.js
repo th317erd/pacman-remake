6737dc5b920227a625b1bec64ad358098d0a6a71
Wyatt Greenway <th317erd@gmail.com>
2018-04-10 20:06:00 -0700

    Map editor working!!!

--- javascript/block.js
+++ javascript/block.js
@@ -2,6 +2,21 @@
   class Block extends root.Actor {
     constructor(...args) {
       super(...args);
+
+      this.classList.push('block');
+      this.updateImageIndex(0);
+    }
+
+    updateImageIndex(index) {
+      this.imageIndex = index;
+      this.imageSource = 'assets/images/map-tiles/' + index + '.svg';
+
+      if (this.element)
+        this.element.setAttribute('src', this.imageSource);
+    }
+
+    orImageIndex(bitmask) {
+      this.updateImageIndex(this.imageIndex | bitmask);
     }
 
     mount(...args) {
--- javascript/editor.js
+++ javascript/editor.js
@@ -13,10 +13,10 @@
       this.sizeX = 15;
       this.sizeY = 15;
 
-      var gridSizeX = this.gridSizeX = 15,
-          gridSizeY = this.gridSizeY = 15,
-          tileSizeX = this.tileSizeX = 3,
-          tileSizeY = this.tileSizeY = 3,
+      var gridSizeX = this.gridSizeX = 15 * 3,
+          gridSizeY = this.gridSizeY = 15 * 3,
+          tileSizeX = this.tileSizeX = 1,
+          tileSizeY = this.tileSizeY = 1,
           tiles = this.tiles = new Array(gridSizeX * gridSizeY);
 
       for (var y = 0, yl = gridSizeY; y < yl; y++) {
@@ -24,17 +24,97 @@
           var actor = tiles[y * gridSizeX + x] = new root.Block({
             x: x * tileSizeX,
             y: y * tileSizeY,
-            imageSource: 'assets/images/map-tiles/15.svg',
-            editor: true
+            imageSource: 'assets/images/map-tiles/15.svg'
           });
         }
       }
 
-      window.addEventListener('mousemove', (event) => {
-        console.log('Mouse Move', event);
-      }, false);
+      window.addEventListener('mousedown', this.mouseEventHandler.bind(this), false);
+      window.addEventListener('mousemove', this.mouseEventHandler.bind(this), false);
 
       this.updateDOM();
+    }
+
+    indexToCoordinates(index) {
+      return {
+        x: index % this.gridSizeX,
+        y: Math.floor(index / this.gridSizeX)
+      };
+    }
+
+    coordinatesToIndex(_x, _y) {
+      var x = (_x % this.gridSizeX) ,
+          y = (_y % this.gridSizeY);
+
+      if (x < 0)
+        x = this.gridSizeX + x;
+
+      if (y < 0)
+        y = this.gridSizeY + y;
+
+      return (y * this.gridSizeX) + x;
+    }
+
+    mouseEventHandler(event) {
+      var actor = event.target._actor;
+      if (!actor)
+        return;
+
+      if (!event.buttons)
+        return;
+
+      if (actor instanceof root.Block) {
+        var blockIndex = this.tiles.indexOf(actor);
+        if (blockIndex < 0)
+          return;
+
+        console.log("OUR BLOCK INDEX IS: ", blockIndex);
+        //????.updateImageIndex(?????);
+        // var l = this.tiles.length,
+        //     topNeighborIndex = (blockIndex < this.gridSizeX)
+        //       ? (l - (this.gridSizeX - (blockIndex % this.gridSizeX)))
+        //       : blockIndex - this.gridSizeX,
+        //     bottomNeighborIndex = (blockIndex >= (l - this.gridSizeX))
+        //       ? (blockIndex % this.gridSizeX)
+        //       : blockIndex + this.gridSizeX,
+        //     leftNeighbor = (blockIndex - 1)
+        //     topNeighbor = this.tiles[topNeighborIndex],
+        //     bottomNeighbor = this.tiles[bottomNeighborIndex];
+
+        // topNeighbor.updateImageIndex(15);
+        // bottomNeighbor.updateImageIndex(15);
+
+        var coords = this.indexToCoordinates(blockIndex),
+            neighbors = [
+              [ -1, -1, 2 ],
+              [ +0, -1, 6 ],
+              [ +1, -1, 4 ],
+              [ +1, +0, 12 ],
+              [ +1, +1, 8 ],
+              [ +0, +1, 9 ],
+              [ -1, +1, 1 ],
+              [ -1, +0, 3 ]
+            ].map((offset) => {
+              var x = offset[0],
+                  y = offset[1],
+                  bitmask = offset[2];
+
+              return {
+                index: this.coordinatesToIndex(coords.x + x, coords.y + y),
+                bitmask
+              };
+            });
+
+        neighbors.forEach((info) => {
+          var index = info.index,
+              bitmask = info.bitmask,
+              neighborActor = this.tiles[index];
+
+          neighborActor.orImageIndex(bitmask);
+        });
+
+        actor.updateImageIndex(15);
+      }
     }
 
     updateDOM() {
